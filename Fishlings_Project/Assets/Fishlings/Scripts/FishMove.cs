8efe3c05f09a0135746c74aef8f4d9b48ac2e7ac
SuperArkonis <80232693+SuperArkonis@users.noreply.github.com>
2021-05-03 09:06:13 +0800

    UI Tweaking for First Build
    Tweaked UI to fit with the placeholder shop art

---
+++ Fishlings_Project/Assets/Fishlings/Scripts/FishMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    public GameObject fish;
+    public float speed = 8;
+    public float distance = 12;
+    float zStartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zStartPos = fish.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((speed < 0 && fish.transform.localPosition.z < zStartPos) || (speed > 0 && fish.transform.localPosition.z > zStartPos + distance))
+        {
+            speed *= -1;
+        }
+        fish.transform.localPosition = new Vector3(fish.transform.localPosition.x, fish.transform.localPosition.y, fish.transform.localPosition.z + speed * Time.deltaTime);
+    }
+}
